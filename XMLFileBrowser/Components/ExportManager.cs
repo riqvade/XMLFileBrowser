ffc3b299b9d0e7c172f4eb31a97a216f3881cd90
riqvade@gmail.com <riqvade@gmail.com>
2022-08-31 12:01:37 +0700

    Рефакторинг

--- XMLFileBrowser/Components/ExportManager.cs
+++ XMLFileBrowser/Components/ExportManager.cs
@@ -29,9 +29,64 @@ namespace XMLFileBrowser.Components
 
             IXLWorkbook workbook = new XLWorkbook();
             IXLWorksheet worksheet = workbook.Worksheets.Add("Sheet1");
-
             ObservableCollection<ChapterModel> chapterModels = fileContentService.GetChapters();
 
+            FormatCells(worksheet, currentColumn);
+
+            foreach (ChapterModel chapter in chapterModels)
+            {
+                if(chapter != null)
+                {
+                    worksheet.Cell(currentRow, currentColumn).Value = chapter.Caption;
+                    worksheet.Row(currentRow).Style.Font.Bold = true;
+                    worksheet.Range(mergeRow).Row(currentRow).Merge();
+                    worksheet.Cell(currentRow, currentColumn).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                    currentRow += 1;
+
+                    foreach (Position position in chapter.Positions)
+                    {
+                        currentColumn += 1;
+                        worksheet.Cell(currentRow, currentColumn).Value = position.Number;
+                        worksheet.Cell(currentRow, currentColumn).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        currentColumn += 1;
+                        worksheet.Cell(currentRow, currentColumn).Value = position.Code;
+                        worksheet.Cell(currentRow, currentColumn).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        currentColumn += 1;
+                        worksheet.Cell(currentRow, currentColumn).Value = position.Caption;
+                        worksheet.Cell(currentRow, currentColumn).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        currentColumn += 1;
+                        worksheet.Cell(currentRow, currentColumn).Value = position.Units;
+                        worksheet.Cell(currentRow, currentColumn).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        currentColumn += 1;
+                        worksheet.Cell(currentRow, currentColumn).Value = position.Quantity + Convert.ToChar(130);
+                        worksheet.Cell(currentRow, currentColumn).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        currentColumn = 1;
+                        currentRow += 1;
+
+                        foreach (Resource resource in position.Resources)
+                        {
+                            currentColumn += 2;
+                            worksheet.Cell(currentRow, currentColumn).Value = resource.Code;
+                            currentColumn += 1;
+                            worksheet.Cell(currentRow, currentColumn).Value = resource.Caption;
+                            currentColumn += 2;
+                            worksheet.Cell(currentRow, currentColumn).Value = resource.Quantity + Convert.ToChar(130);
+
+                            currentColumn = 1;
+                            currentRow += 1;
+                        }
+                    }
+                }
+            }
+            workbook.SaveAs(filePath);
+            MessageBox.Show("Файл создан");
+        }
+
+        /// <summary>
+        /// Форматирует ячейки
+        /// </summary>
+        private static void FormatCells(IXLWorksheet worksheet, int currentColumn)
+        {
             worksheet.Column(currentColumn).Width = 3;
             currentColumn += 1;
             worksheet.Column(currentColumn).Width = 7;
@@ -56,49 +111,6 @@ namespace XMLFileBrowser.Components
             worksheet.Column(currentColumn).Width = 12;
             worksheet.Column(currentColumn).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
             worksheet.Column(currentColumn).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-            currentColumn = 1;
-
-            foreach (ChapterModel chapter in chapterModels)
-            {
-                if(chapter != null)
-                {
-                    worksheet.Cell(currentRow, currentColumn).Value = chapter.Caption;
-                    worksheet.Row(currentRow).Style.Font.Bold = true;
-                    worksheet.Range(mergeRow).Row(currentRow).Merge();
-                    currentRow += 1;
-
-                    foreach (Position position in chapter.Positions)
-                    {
-                        currentColumn += 1;
-                        worksheet.Cell(currentRow, currentColumn).Value = position.Number;
-                        currentColumn += 1;
-                        worksheet.Cell(currentRow, currentColumn).Value = position.Code;
-                        currentColumn += 1;
-                        worksheet.Cell(currentRow, currentColumn).Value = position.Caption;
-                        currentColumn += 1;
-                        worksheet.Cell(currentRow, currentColumn).Value = position.Units;
-                        currentColumn += 1;
-                        worksheet.Cell(currentRow, currentColumn).Value = position.Quantity;
-                        currentColumn = 1;
-                        currentRow += 1;
-
-                        foreach (Resource resource in position.Resources)
-                        {
-                            currentColumn += 2;
-                            worksheet.Cell(currentRow, currentColumn).Value = resource.Code;
-                            currentColumn += 1;
-                            worksheet.Cell(currentRow, currentColumn).Value = resource.Caption;
-                            currentColumn += 2;
-                            worksheet.Cell(currentRow, currentColumn).Value = resource.Quantity;
-
-                            currentColumn = 1;
-                            currentRow += 1;
-                        }
-                    }
-                }
-            }
-            workbook.SaveAs(filePath);
-            MessageBox.Show("Файл создан");
         }
     }
 }
